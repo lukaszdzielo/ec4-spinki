7100927e79a6e34b8ad62beaecfda287b8a25351
Łukasz Dzieło <lukasz.dzielo@gmail.com>
2020-10-16 14:21:48 +0200

    referencje

---
+++ elements/testimonials_section.php
@@ -0,0 +1,30 @@
+<section class="pt-30 pb-80 overflow-hidden">
+    <div class="container">
+        <div class="row gx-50">
+            <div class="col-lg-6 mb-30">
+                <div class="position-relative pl-40 pt-130 pb-50">
+                    <div class="shape__custom--1">
+                        <svg class="svg__custom--1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 520 446.86"><path d="M292.57,36.57V256H438.86c0,80.66-65.63,146.29-146.29,146.29v73.14C413.57,475.43,512,377,512,256V36.57Z" transform="translate(4 -32.57)" style="fill:none;stroke:#231f20;stroke-miterlimit:10;stroke-width:8px"/><path d="M0,256H146.29c0,80.66-65.63,146.29-146.29,146.29v73.14C121,475.43,219.43,377,219.43,256V36.57H0Z" transform="translate(4 -32.57)" style="fill:none;stroke:#231f20;stroke-miterlimit:10;stroke-width:8px"/></svg>
+                    </div>
+                    <div class="mb-30">
+                        <div class="h1 m-0">REFERENCJE</div>
+                    </div>
+                    <p>W 14 lat zorganizowaliśmy ponad 500 wesel. Od samego początku referencje od naszych par są dla nas powodem do dumy.</br></br>Nie jesteśmy w stanie przedstawić wszystkich referencji od naszych zadowolonych par, dlatego wyselekcjonowaliśmy tylko kilka z nich. Zachęcamy Cię do przeczytania opinii o weselach ze Spinkami.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 d-flex flex-column mb-30">
+                <div class="mt-lg-130 mb-auto">
+                    <div class="testimonials mt-lg-70">
+                        <div class="testimonial mb-30">
+                            <div class="testimonial--text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim ligula nec maximus pharetra. Morbi quis risus in arcu tincidunt posuere vitae non dui. Sed consectetur velit tellus.</p>
+                            </div>
+                            <div class="testimonial--author">Imię i nazwisko</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="line__custom--1"></div>
+            </div>
+        </div>
+    </div><!-- /.container -->
+</section>
--- index.php
+++ index.php
@@ -5,26 +5,24 @@
 <?php include 'elements/_header.php' ?>
 
 <main>
-    <section class="">
-        <div class="container">
-            <div class="starter-template py-55">
-                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-            </div>
-        </div><!-- /.container -->
-    </section>
 
-    
-    <section class="bg-light ">
-        <div class="container">
-            <div class="pt-50 pb-110">
-                <div class="h1 m-0">REFERENCJE</div>
-                <p>Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-            </div>
-        </div><!-- /.container -->
-    </section>
+<section class="">
+    <div class="container">
+        <div class="starter-template py-55">
+            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        </div>
+    </div><!-- /.container -->
+</section>
 
-    <?php include 'elements/team_carousel.php' ?>
-    <?php include 'elements/portfolio_blog_carousel.php' ?>
+<div class="bg-light" style="width: 100%; height: 10px;"></div>
+
+<?php include 'elements/testimonials_section.php' ?>
+
+<div class="bg-light" style="width: 100%; height: 10px;"></div>
+
+<?php include 'elements/team_section.php' ?>
+<?php include 'elements/portfolio_blog_section.php' ?>
+
 </main>
 
 <?php include 'elements/_footer.php' ?>
